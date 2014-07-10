79ffafd720536151c9c74885dad17840b7faee3d
Willem Larsen <willemlarsen@gmail.com>
2014-07-10 15:20:43 -0700

    remove multiples of a given number from a number list

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -2,3 +2,11 @@ def create_number_list(number)
 	number_list = (1..number).to_a
 	number_list 
 end
+
+def remove_multiples_of_a_number(number, list_of_numbers)
+	new_list = list_of_numbers.reject do |list_item| 
+		list_item % 2 == 0 
+	end
+	new_list
+end
+
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -7,4 +7,11 @@ describe "prime_sifting" do
 			expect(create_number_list(5)).to eq([1, 2, 3, 4, 5])
 		end
 	end
+	describe "remove_multiples_of_a_number" do 
+		it "filters out multiples of a given number from a number list" do 
+			expect(remove_multiples_of_a_number(2, [1, 2, 3, 4, 5])).to eq(
+				[1, 3, 5])
+		end
+	end
+	
 end
